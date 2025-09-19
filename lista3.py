410760fff115ebeb51436555cfb7ec1011426f36
CristopherAlejandro27 <L251050184@pabellon.tecnm.mx>
2025-09-19 08:56:29 -0600

    Create lista3.py

---
+++ lista3.py
@@ -0,0 +1,28 @@
+print("lista de Cris:")
+lista_Cris = []
+
+nombre = input("Nombre: ")
+lista_Cris.append(nombre)
+
+#Agregar producto
+Preparatoria = input("Preparatoria: ")
+lista_Cris.append(Preparatoria)
+
+#Agregar producto
+Residencia = input("Residencia: ")
+lista_Cris.append(Residencia)
+
+#Agregar producto
+Edad = input("Edad: ")
+lista_Cris.append(Edad)
+
+#Agregar producto
+Carrera = input("Carrera: ")
+lista_Cris.append(Carrera)
+
+print("\n📌 Tu lista es: ")
+for elementos in lista_Cris:
+    print(f"- {elementos}")
+
+print("\n✅ ¡Lista creada con éxito!")
+#https://www.webfx.com/tools/emoji-cheat-sheet/
